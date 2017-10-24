e7781c17e86a17eeb8725b46a2c2fe925ad27d3b
Wojtek <wojtekgomola@gmail.com>
2017-10-24 15:22:25 +0200

    some changes

---
+++ MainApp/Models/Contract.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace MainApp.Models
+{
+    public class Contract :IBson
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public List<WantedPerson> WantedPersons { get; set; }
+        public string ContractStatus{ get; set; }
+    }
+}
---
+++ MainApp/Models/WantedPerson.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MainApp.Models
+{
+    public class WantedPerson
+    {
+        public string Price { get; set; }
+        public string Availability { get; set; }
+        public List<Skill> Skills { get; set; }
+        public string Status { get; set; }
+    }
+}
