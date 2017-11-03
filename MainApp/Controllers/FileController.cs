6c9e65256ef07be9af9aabb04a4e296e486de6cd
Wojtek <wojtekgomola@gmail.com>
2017-11-03 14:31:25 +0100

    Kontroler file wstępnie ukończony.

--- MainApp/Controllers/FileController.cs
+++ MainApp/Controllers/FileController.cs
@@ -1,18 +1,21 @@
 ﻿using MainApp.Interfaces;
 using MainApp.Models;
 using MainApp.Models.Entities;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
 namespace MainApp.Controllers
 {
+    [RoutePrefix("api/file")]
     public class FileController : ApiController
     {
         IRepository<Person> repository = new Repository<Person>("persons");
@@ -23,33 +26,47 @@ namespace MainApp.Controllers
         }
 
         // GET: api/File/5
-        public string Get(int id)
+        [Route("{id}")]
+        public HttpResponseMessage Get(string id)
         {
-            return "value";
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/tempFiles/");
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            var stream = new System.IO.MemoryStream();
+            var fileBytes  =  FileTools.GetFile(repository, new ObjectId(id));
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(root);
+            foreach (System.IO.FileInfo file in directory.GetFiles()) file.Delete();
+            using (Stream file = System.IO.File.Create(root+"lang.png", (int)fileBytes.Length))
+            {
+                byte[] bytesInStream = new byte[stream.Length];
+                stream.Read(fileBytes, 0, fileBytes.Length);
+                file.Write(fileBytes, 0, fileBytes.Length);
+            }
+
+            response.Content = new StreamContent(new FileStream(root+"lang.png", FileMode.Open,FileAccess.Read));
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            response.Content.Headers.ContentLength = fileBytes.Length;
+            response.Content.Headers.ContentDisposition.FileName = "language.png";
+            response.Content.Headers.ContentDisposition.Size = fileBytes.Length;
+            return response;
+            
         }
 
         // POST: api/File
         public async Task<string> Post()
         {
             string fileId = "";
-            string root = HttpContext.Current.Server.MapPath("~/App_Data/tempFiles/");
             try
             {
-
-                var provider = new MultipartFormDataStreamProvider(root);
-                var filedata = await Request.Content.ReadAsMultipartAsync(provider);
-                foreach (var fileItem in provider.FileData)
+                if (!Request.Content.IsMimeMultipartContent())
+                    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                var provider = new MultipartMemoryStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+                foreach (var fileItem in provider.Contents)
                 {
-                    FileStream stream = System.IO.File.Open(fileItem.LocalFileName, FileMode.Open);
-                    var fileName= fileItem.Headers.ContentDisposition.FileName;
-                    fileName=  fileName.Trim().Substring(1, fileName.Length - 2);
-                    fileId = FileTools.SaveFile(repository, stream, fileName).ToString();
-                    System.IO.File.SetAttributes(fileItem.LocalFileName, FileAttributes.Normal);
-                    if (stream != null)
-                    {
-                        stream.Dispose();
-                    }
-                    System.IO.File.Delete(fileItem.LocalFileName);
+                    var fileName = fileItem.Headers.ContentDisposition.FileName.Trim('\"');
+                    var buffer = await fileItem.ReadAsStreamAsync();
+                    fileId = FileTools.SaveFile(repository, buffer, fileName).ToString();
                 }
             }
             catch (IOException ex)
@@ -63,10 +80,11 @@ namespace MainApp.Controllers
         public void Put(int id, [FromBody]string value)
         {
         }
-
+        [Route("{id}")]
         // DELETE: api/File/5
-        public void Delete(int id)
+        public void Delete(string id)
         {
+            FileTools.DeleteFile(repository,new ObjectId(id));
         }
     }
 }
--- MainApp/Models/FileTools.cs
+++ MainApp/Models/FileTools.cs
@@ -7,17 +7,32 @@ using System.Linq;
 using System.Web;
 using MongoDB.Driver.GridFS;
 using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections;
 
 namespace MainApp.Models
 {
-    public class FileTools
+    public static class FileTools
     {
         static IGridFSBucket bucket;
-        public static ObjectId SaveFile(IRepository<Person> repository,FileStream stream, string fileName)
+        public static ObjectId SaveFile(IRepository<Person> repository, Stream stream, string fileName)
         {
             bucket = new GridFSBucket(repository.Database);
-            ObjectId id = bucket.UploadFromStream(fileName,stream);
+            ObjectId id = bucket.UploadFromStream(fileName, stream);
             return id;
+        }
+
+        public static byte[] GetFile(IRepository<Person> repository, ObjectId id)
+        {
+            bucket = new GridFSBucket(repository.Database);
+            //bucket.DownloadToStream(id, stream);
+            var bytes = bucket.DownloadAsBytes(id);
+            return bytes;
+        }
+        public static void DeleteFile(IRepository<Person> repository, ObjectId id)
+        {
+            bucket = new GridFSBucket(repository.Database);
+            bucket.Delete(id);
         }
 
     }
